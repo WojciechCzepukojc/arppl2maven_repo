f8d542c823e09a01d35fc56a3e3777260d87e017
Wojciech Czepukojć <wojciechczepukojc@gmail.com>
2022-04-03 13:55:35 +0200

    Komentarz

---
+++ src/main/java/pl/sda/arppl2/maven/Main.java
@@ -0,0 +1,11 @@
+package pl.sda.arppl2.maven;
+
+public class Main {
+    //psvm - public static void main - generuje metodę starową
+    public static void main(String[] args) {
+//Generuję komendę wpisania do konsoli
+        // sout -> System.out.println();
+        System.out.println("Hello World!");
+
+    }
+}
